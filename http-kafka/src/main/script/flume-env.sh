57664ed87a0dbd62da21a500bbc8e5064602deae
彭明 <pengming@admaster.com.cn>
2016-12-12 15:22:53 +0800

    正常停止和重启flume

--- http-kafka/src/main/script/flume-env.sh
+++ http-kafka/src/main/script/flume-env.sh
@@ -22,7 +22,7 @@
 # export JAVA_HOME=/usr/lib/jvm/java-6-sun
 
 # Give Flume more memory and pre-allocate, enable remote monitoring via JMX
-export JAVA_OPTS="-Xms2048m -Xmx2048m -XX:MaxDirectMemorySize=256m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -Dcom.sun.management.jmxremote"
+export JAVA_OPTS="-server -Xms2048m -Xmx2048m -XX:MaxDirectMemorySize=256m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -Dcom.sun.management.jmxremote"
 
 # Let Flume write raw event data and configuration information to its log files for debugging
 # purposes. Enabling these flags is not recommended in production,
--- http-kafka/src/main/script/service.sh
+++ http-kafka/src/main/script/service.sh
@@ -1,9 +1,9 @@
  #!/bin/bash
  #--------------------------------------------------------------------
  #作者：ming.peng
- #日期：2012-12-18
- #参数：
- #功能：flume程序的启停
+ #日期：2016-12-12
+ #参数：start|stop|restart
+ #功能：flume 启动停止重启
  #-------------------------------------------------------------------
 
 path=$(cd `dirname $0`; pwd)
@@ -20,7 +20,7 @@ function start(){
        #eval nohup java -Xmx512m -jar -DplanNames=$planNames -DconfigPath=$CONFIG_PATH $jarpath/$JAR `echo $@|cut -d " " -f3-$#` >> /dev/null 2>&1 &
        eval nohup flume-ng agent -c $path/conf -f $path/conf/http-test.conf -n agent -Dflume.root.logger=INFO,LOGFILE -Dflume.log.dir=$path/logs -Dflume.monitoring.type=http -Dflume.monitoring.port=34545 >> /dev/null 2>&1 &
        echo "启动成功...."
-       tail -f $path/logs/flume.log
+       echo "日志路径: $path/logs/flume.log"
     else
        echo "进程已经存在，启动失败，请检查....."
        exit 0
@@ -28,42 +28,48 @@ function start(){
 }
 
 function stop(){
-   echo "开始stop ....."
-   num=`ps -ef|grep java|grep $JAR|wc -l`
-   if [ "$num" != "0" ] ; then
+    echo "开始stop ....."
+    num=`ps -ef|grep java|grep $JAR|wc -l`
+    if [ "$num" != "0" ] ; then
      #ps -ef|grep java|grep $JAR|awk '{print $2;}'|xargs kill -9
      # 正常停止flume
      ps -ef|grep java|grep $JAR|awk '{print $2;}'|xargs kill
      echo "进程已经关闭..."
-   else
+    else
      echo "服务未启动，无需停止..."
-   fi
+    fi
 }
 
 
 function restart(){
- echo "begin stop process ..."
- stop
- echo "process stoped,and starting ..."
- start
- echo "started ..."
+    echo "begin stop process ..."
+    stop
+    # 判断程序是否彻底停止
+    num=`ps -ef|grep java|grep $JAR|wc -l`
+    while [ $num -gt 0 ]; do
+        sleep 1
+        num=`ps -ef|grep java|grep $JAR|wc -l`
+    done
+    echo "process stoped,and starting ..."
+    start
+    echo "started ..."
 }
 
 case "$1" in
-   "start")
+    "start")
       start $@
       exit 0
     ;;
-   "stop")
+    "stop")
       stop
       exit 0
      ;;
-   "restart")
+    "restart")
        restart
        exit 0
      ;;
-   *)
-       echo "用法： $0 {start|stop|restart default|资源后缀:资源后缀... 任务ID 任务ID...}"
+    *)
+       echo "用法： $0 {start|stop|restart}"
        exit 1
     ;;
 esac
