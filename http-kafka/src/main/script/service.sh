0933e6d439ffea3d7b20609534036fce8f111061
彭明 <pengming@admaster.com.cn>
2016-12-12 11:24:35 +0800

    正常停止flume

--- http-kafka/src/main/script/service.sh
+++ http-kafka/src/main/script/service.sh
@@ -31,7 +31,9 @@ function stop(){
    echo "开始stop ....."
    num=`ps -ef|grep java|grep $JAR|wc -l`
    if [ "$num" != "0" ] ; then
-     ps -ef|grep java|grep $JAR|awk '{print $2;}'|xargs kill -9
+     #ps -ef|grep java|grep $JAR|awk '{print $2;}'|xargs kill -9
+     # 正常停止flume
+     ps -ef|grep java|grep $JAR|awk '{print $2;}'|xargs kill
      echo "进程已经关闭..."
    else
      echo "服务未启动，无需停止..."
